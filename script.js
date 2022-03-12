392663ad6e5d01b7f9c4ac68d67c1582307da3f4
Dafina Beqiraj <93763212+DaffeB@users.noreply.github.com>
2022-03-12 20:36:21 +0100

    CSS &  JS - Open when someone clicks on the span element

--- script.js
+++ script.js
@@ -163,3 +163,120 @@ function clearBtn() {
     }
 
 }
+
+getMovies(API_URL);
+
+function getMovies(url) {
+    lastUrl = url;
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data.results)
+        if (data.results.length !== 0) {
+            showMovies(data.results);
+            currentPage = data.page;
+            nextPage = currentPage + 1;
+            prevPage = currentPage - 1;
+            totalPages = data.total_pages;
+
+            current.innerText = currentPage;
+
+            if (currentPage <= 1) {
+                prev.classList.add('disabled');
+                next.classList.remove('disabled')
+            } else if (currentPage >= totalPages) {
+                prev.classList.remove('disabled');
+                next.classList.add('disabled')
+            } else {
+                prev.classList.remove('disabled');
+                next.classList.remove('disabled')
+            }
+
+            tagsEl.scrollIntoView({ behavior: 'smooth' })
+
+        } else {
+            main.innerHTML = `<h1 class="no-results">No Results Found</h1>`
+        }
+
+    })
+
+}
+
+function showMovies(data) {
+    main.innerHTML = '';
+
+    data.forEach(movie => {
+        const { title, poster_path, vote_average, overview, id } = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+             <img src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580" }" alt="${title}">
+
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getColor(vote_average)}">${vote_average}</span>
+            </div>
+
+            <div class="overview">
+
+                <h3>Overview</h3>
+                ${overview}
+                <br/> 
+                <button class="know-more" id="${id}">Know More</button
+            </div>
+        
+        `
+
+        main.appendChild(movieEl);
+
+        document.getElementById(id).addEventListener('click', () => {
+            console.log(id)
+            openNav(movie)
+        })
+    })
+}
+
+const overlayContent = document.getElementById('overlay-content');
+/* Open when someone clicks on the span element */
+function openNav(movie) {
+    let id = movie.id;
+    fetch(BASE_URL + '/movie/' + id + '/videos?' + API_KEY).then(res => res.json()).then(videoData => {
+        console.log(videoData);
+        if (videoData) {
+            document.getElementById("myNav").style.width = "100%";
+            if (videoData.results.length > 0) {
+                var embed = [];
+                var dots = [];
+                videoData.results.forEach((video, idx) => {
+                    let { name, key, site } = video
+
+                    if (site == 'YouTube') {
+
+                        embed.push(`
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/${key}" title="${name}" class="embed hide" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          
+          `)
+
+                        dots.push(`
+              <span class="dot">${idx + 1}</span>
+            `)
+                    }
+                })
+
+                var content = `
+        <h1 class="no-results">${movie.original_title}</h1>
+        <br/>
+        
+        ${embed.join('')}
+        <br/>
+
+        <div class="dots">${dots.join('')}</div>
+        
+        `
+                overlayContent.innerHTML = content;
+                activeSlide = 0;
+                showVideos();
+            } else {
+                overlayContent.innerHTML = `<h1 class="no-results">No Results Found</h1>`
+            }
+        }
+    })
+}
